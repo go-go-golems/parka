69d82e2a72450249c320e4f6645e6fdd91bab765
Manuel Odendahl <wesen@ruinwesen.com>
2024-01-17 17:36:09 +0100

    :ambulance: Fix passing middlewares to the json query handler as well

--- pkg/handlers/command/command.go
+++ pkg/handlers/command/command.go
@@ -226,10 +226,14 @@ func NewCommandHandlerFromConfig(
 func (ch *CommandHandler) Serve(server *parka.Server, path string) error {
 	path = strings.TrimSuffix(path, "/")
 
-	server.Router.GET(path+"/data", func(c *gin.Context) {
-		json.CreateJSONQueryHandler(ch.Command)(c)
-	})
 	middlewares_ := ch.ParameterFilter.ComputeMiddlewares(ch.Stream)
+
+	server.Router.GET(path+"/data", func(c *gin.Context) {
+		options := []json.QueryHandlerOption{
+			json.WithMiddlewares(middlewares_...),
+		}
+		json.CreateJSONQueryHandler(ch.Command, options...)(c)
+	})
 	server.Router.GET(path+"/glazed", func(c *gin.Context) {
 		options := []datatables.QueryHandlerOption{
 			datatables.WithMiddlewares(middlewares_...),
